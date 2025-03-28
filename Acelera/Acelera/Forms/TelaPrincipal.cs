b4b7b5d9ad54332b702cc706451b0ef0147344b8
Pedro <pedro.fialho000@gmail.com>
2025-03-28 18:56:08 -0300

    Adicionei tela Principal e Perfil

---
+++ Acelera/Acelera/Forms/TelaPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Acelera.Forms
+{
+    public partial class TelaPrincipal: Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TelaPerfil telaPerfil = new TelaPerfil();
+            telaPerfil.Show(); 
+        }
+    }
+}
