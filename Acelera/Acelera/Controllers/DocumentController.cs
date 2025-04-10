511383fdd763543b5e24caf046b30cab0a07260f
Pedro Fialho <pedrofialho000@gmail.com>
2025-04-10 08:24:22 -0300

    Adicionando Controllers

---
+++ Acelera/Acelera/Controllers/DocumentController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Acelera.Controllers
+{
+    internal class DocumentController
+    {
+        public static bool VerificaCNPJ(string cnpj)
+        {
+            cnpj = Regex.Replace(cnpj, "[^0-9]", "");
+
+            if(cnpj.Length != 14)
+                return false;
+
+            if(new string(cnpj[0], cnpj.Length) == cnpj)
+                return false;
+
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCnpj = cnpj.Substring(0, 12);
+            int soma = 0;
+
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+            resto = resto < 2 ? 0 : 11 - resto;
+
+            string digito = resto.ToString();
+            tempCnpj += digito;
+
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            resto = resto < 2 ? 0 : 11 - resto;
+
+            digito += resto.ToString();
+
+            return cnpj.EndsWith(digito);
+        }
+
+        public static bool ValidarCPF(string cpf)
+        {
+            cpf = Regex.Replace(cpf, "[^0-9]", "");
+
+            if (cpf.Length != 11)
+                return false;
+
+            if (new string(cpf[0], cpf.Length) == cpf)
+                return false;
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
+
+            int resto = soma % 11;
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
+
+            resto = soma % 11;
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+
+            string digitosVerificadores = cpf.Substring(9, 2);
+            string digitosCalculados = digito1.ToString() + digito2.ToString();
+
+            return digitosVerificadores == digitosCalculados;
+        }
+    }
+}
---
+++ Acelera/Acelera/Controllers/PasswordController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Acelera.Controllers
+{
+    internal class PasswordController
+    {        
+    private Byte[] salt = new byte[16];
+        public Boolean VerificarSenha(String senha)
+        {
+            Boolean caractereEspecial = Regex.IsMatch(senha, "[@#!$%&]");
+            Boolean caractereNumerico = Regex.IsMatch(senha, "[0-9]");
+            Boolean caractereMaiusculo = Regex.IsMatch(senha, "[A-Z]");
+            Boolean tamanho = senha.Length >= 8 ? true : false;
+            if (caractereEspecial && caractereNumerico && caractereMaiusculo && tamanho)
+            {
+                System.Diagnostics.Debug.WriteLine("senha valida");
+                return true;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("senha fornecida não atende aos padroes pedidos");
+                return false;
+            }
+        }
+        
+    }
+}
