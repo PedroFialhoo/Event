28b3778d158b488effa12717077f1559395b322e
Pedro <pedro.fialho000@gmail.com>
2025-04-28 22:19:46 -0300

    Adicionando a tela perfil colaborador

--- Acelera/Acelera/Views/TelaPerfilColaborador.Designer.cs
+++ Acelera/Acelera/Views/TelaPerfilColaborador.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picturePerfil = new System.Windows.Forms.PictureBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblNomeEmpresa = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePerfil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picturePerfil
+            // 
+            this.picturePerfil.Location = new System.Drawing.Point(48, 220);
+            this.picturePerfil.Name = "picturePerfil";
+            this.picturePerfil.Size = new System.Drawing.Size(300, 325);
+            this.picturePerfil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picturePerfil.TabIndex = 0;
+            this.picturePerfil.TabStop = false;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.BackColor = System.Drawing.Color.Transparent;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F);
+            this.lblNome.Location = new System.Drawing.Point(443, 274);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(79, 29);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "label1";
+            this.lblNome.Click += new System.EventHandler(this.lblNome_Click);
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.BackColor = System.Drawing.Color.Transparent;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F);
+            this.lblTelefone.Location = new System.Drawing.Point(1121, 257);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(79, 29);
+            this.lblTelefone.TabIndex = 8;
+            this.lblTelefone.Text = "label1";
+            // 
+            // lblNomeEmpresa
+            // 
+            this.lblNomeEmpresa.AutoSize = true;
+            this.lblNomeEmpresa.BackColor = System.Drawing.Color.Transparent;
+            this.lblNomeEmpresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F);
+            this.lblNomeEmpresa.Location = new System.Drawing.Point(443, 477);
+            this.lblNomeEmpresa.Name = "lblNomeEmpresa";
+            this.lblNomeEmpresa.Size = new System.Drawing.Size(79, 29);
+            this.lblNomeEmpresa.TabIndex = 9;
+            this.lblNomeEmpresa.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(1344, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(343, 95);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TelaPerfilColaborador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TelaPerfilColaborador";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::Acelera.Properties.Resources.TelaPerfilColaborador;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblNomeEmpresa);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.picturePerfil);
+            this.MaximizeBox = false;
+            this.Name = "TelaPerfilColaborador";
+            this.Text = "Event";
+            this.Load += new System.EventHandler(this.TelaPerfilColaborador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picturePerfil)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picturePerfil;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblNomeEmpresa;
+        private System.Windows.Forms.Button button2;
     }
 }
